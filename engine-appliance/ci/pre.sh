2de9a69022b48d7c6a433a52b7a727c231b1cf46
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-18 10:59:16 +0100

    engine: Add oz dependency
    Change-Id: Id0fc05e851510e3372bfdf69d7734d7a9a892659
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- engine-appliance/ci/pre.sh
+++ engine-appliance/ci/pre.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-sudo yum -y install libguestfs-tools qemu-system-x86 asciidoc python-sh glusterfs gluster squashfs-tools
+sudo yum -y install libguestfs-tools qemu-system-x86 asciidoc python-sh glusterfs gluster squashfs-tools oz
 git submodule update --init --recursive
