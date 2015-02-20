afb0890b18fe83e62f5d4725b151f0342fbdfc5f
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-20 10:01:04 +0100

    Update to later image-tools
    Change-Id: I00bf0fd2543cbb1c22689e682ae4da62b2fbdb08
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- engine-appliance/ci/pre.sh
+++ engine-appliance/ci/pre.sh
@@ -5,5 +5,5 @@ set -ex
 sudo yum -y install libguestfs-tools qemu-system-x86 asciidoc python-sh glusterfs gluster squashfs-tools oz
 
 pushd ..
-git submodule update --init --recursive
+git submodule update --init --recursive --force
 popd
