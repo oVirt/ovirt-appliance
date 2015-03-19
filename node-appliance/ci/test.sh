d338ff046b3a59604392f0dc6a1a1bf038baeaa2
Fabian Deutsch <fabiand@fedoraproject.org>
2015-03-19 10:43:43 +0100

    node: Save both anacond logs
    Change-Id: I6032a417f6b45396147644284ce302f139bf476e
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- node-appliance/ci/test.sh
+++ node-appliance/ci/test.sh
@@ -7,6 +7,8 @@ export PATH=$PATH:/sbin/:/usr/sbin/
 make ovirt-node-appliance.squashfs.img
 [[ -n "$SQUASHFS_URL" ]] && make image-install SQUASHFS_URL=$SQUASHFS_URL || make image-install
 
+mv anaconda.log anaconda-install.log
+
 IMG="$(make verrel).squashfs.img"
 ln -v ovirt-node-appliance.squashfs.img $IMG
 
