bd760c639c59a0dc70937cf7508880f4a3a2f0f8
Fabian Deutsch <fabiand@fedoraproject.org>
2015-03-20 09:20:18 +0100

    node: Add automation directory
    Change-Id: I9e0cb519ca2933356213c960dc8dcfcec06be4f6
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- node-appliance/ci/pre.sh
+++ node-appliance/ci/pre.sh
@@ -5,5 +5,4 @@ set -ex
 export PATH=$PATH:/sbin:/usr/sbin
 export TMPDIR=/var/tmp/
 
-sudo yum -y install libguestfs-tools qemu-system-x86 asciidoc python-sh glusterfs gluster squashfs-tools
 git submodule update --init --recursive
