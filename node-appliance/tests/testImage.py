69ca9d6e6556ec9a9ab4e8cfd1f455cd6fb3578a
Fabian Deutsch <fabiand@fedoraproject.org>
2015-03-21 11:48:49 +0100

    node: Fix pkg check
    Change-Id: I5ddb1a9585e3a2092a0b2abc32fc9e6933a68ff6
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- node-appliance/tests/testImage.py
+++ node-appliance/tests/testImage.py
@@ -13,11 +13,14 @@ from unittest import SkipTest
 # Increase the capture length of python-sh to show complete errors
 sh.ErrorReturnCode.truncate_cap = 999999
 
+
 def log(x):
     print(x)
 
 
 def lines(data):
+    """This method is not suited for big data
+    """
     return [l for l in data.split("\n") if l]
 
 
@@ -30,11 +33,17 @@ class TestRootfsQcow2Image(unittest.TestCase):
     sh = _fish.bake("sh")
 
     def test_package(self):
-        all_pkgs = lines(self.sh("rpm -qa"))
-        # log("All packages: %s" % all_pkgs)
+        missing_pkgs = []
+        all_pkgs = lines(self.sh("rpm -qa --qf '%{NAME}\\n'"))
+
         for rpkg in ["vdsm", "cockpit", "glusterfs-server"]:
-            assert rpkg in all_pkgs, "%s is missing" % rpkg
-            log("%s is in the image")
+            if rpkg in all_pkgs:
+                log("%s is in the image" % rpkg)
+            else:
+                missing_pkgs.append(rpkg)
+
+        assert len(missing_pkgs) == 0, \
+            "Some packages are missing: %s" % missing_pkgs
 
     def test_partitioning(self):
         pt = lines(self._fish("list-partitions"))
@@ -81,4 +90,4 @@ class TestRootfsSquashfsImage(unittest.TestCase):
         """Check if the squashfs tree is valid
         """
         raise SkipTest("Not implemented")
-        # FIXME check if /boot etc exists
+        # FIXME check if /boot /sbin etc exists
