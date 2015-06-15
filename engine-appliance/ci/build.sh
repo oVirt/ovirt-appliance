7c89d3e80d2392b9f94b572fe2a7b8ba3c874016
Fabian Deutsch <fabiand@fedoraproject.org>
2015-06-15 14:34:06 +0200

    engine: Drop squashfs generation
    Engine can be greatly distributed using containers, so no need to ship
    it in a squashfs img.
    
    Change-Id: Iaa3991161ec104fd6a883a24605e29edda1c1bbd
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- engine-appliance/ci/build.sh
+++ engine-appliance/ci/build.sh
@@ -6,8 +6,3 @@ export TMPDIR=/var/tmp/
 
 # Create the OVA
 make
-
-# Create the squashfs for liveimg installation
-make ovirt-engine-appliance.squashfs.img
-
-ln -v ovirt-engine-appliance.squashfs.img $(make verrel).squashfs.img
