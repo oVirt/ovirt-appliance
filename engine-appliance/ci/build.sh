08d3aa692fb3f72d02a218e2c0d1931f935b09b1
Fabian Deutsch <fabiand@fedoraproject.org>
2015-06-24 16:56:24 +0200

    engine: Build the ova
    Change-Id: I33f8b2299600d2adb334cd5080570cb71fc30422
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- engine-appliance/ci/build.sh
+++ engine-appliance/ci/build.sh
@@ -5,9 +5,4 @@ export PATH=$PATH:/sbin:/usr/sbin
 export TMPDIR=/var/tmp/
 
 # Create the OVA
-#make
-
-# Create the squashfs for liveimg installation
-make ovirt-engine-appliance.squashfs.img
-
-ln -v ovirt-engine-appliance.squashfs.img $(make verrel).squashfs.img
+make
