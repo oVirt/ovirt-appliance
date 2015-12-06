1c76b9a27ba96e40271a98c20a3f9029bac9572c
Tolik Litovsky <tlitovsk@redhat.com>
2015-12-06 14:58:48 +0200

    Dropping node-appliance
    It moved to ovirt-node-ng on gerrit.ovirt.org
    
    Signed-off-by: Tolik Litovsky <tlitovsk@redhat.com>
    Change-Id: I2867288648014a95074b0e66faab7a6f542de80c

--- node-appliance/ci/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-set -ex
-
-export PATH=$PATH:/sbin:/usr/sbin
-export TMPDIR=/var/tmp/
-
-make image-build ovirt-node-appliance-manifest-rpm
--- node-appliance/ci/pre.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-set -ex
-
-export PATH=$PATH:/sbin:/usr/sbin
-export TMPDIR=/var/tmp/
-
-git submodule update --init --recursive
