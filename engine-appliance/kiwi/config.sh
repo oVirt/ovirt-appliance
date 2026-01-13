980bc28738109208817148b25356f849dff8a342
Jean-Louis Dupond <jean-louis@dupond.be>
2026-01-13 12:16:03 +0100

    kiwi: fix build for CBS
    - Point to the Alma version of centos-release-ovirt45
    - Fix setting firewall config
    - Authselect on EL10 is now 'local'
    - Enable oVirt repositories on CBS build
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- engine-appliance/kiwi/config.sh
+++ engine-appliance/kiwi/config.sh
@@ -1,9 +1,21 @@
-authselect select minimal
-firewall --enabled --service=cockpit
+if [[ $(rpm --eval '%{centos_ver}') == '9' ]]; then
+    authselect select minimal
+else
+    authselect select local
+fi
+
+systemctl enable firewalld
 systemctl enable sshd
 systemctl enable fstrim.timer
 systemctl enable cockpit.socket
 systemctl enable qemu-guest-agent
+firewall-offline-cmd --add-service=cockpit
+
+if [[ "$kiwi_profiles" == *"cbs"* ]]; then
+    # Enable oVirt Testing Repository
+    dnf config-manager --set-enabled centos-ovirt45-testing
+    dnf config-manager --set-enabled ovirt-45-upstream-testing
+fi
 
 # Install oVirt Packages
 dnf -y install ovirt-engine \
